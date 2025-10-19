b618146408aca926e26e66df97cda713cb7ac0f9
olgs0011 <144258663+olgs0011@users.noreply.github.com>
2025-10-19 18:21:39 +0500

    Update blogData.js

--- src/js/mockData/blogData.js
+++ src/js/mockData/blogData.js
@@ -1,38 +1,41 @@
 export const blogData = {
-    largeBlock: {
-        imgSrc: './assets/img/1_4.png',
-        alt: 'Большая иллюстрация к статье о GPT-4 и Openai',
-        date: 'Сент 26, 2021',
-        text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
-        link: '#',
-    },
+    pageTitle: "Многое Происходит, Мы Ведем об Этом Блог.",
+    largeBlocks: [
+        {
+            imgSrc: './assets/img/1_4.png',
+            alt: 'Большая иллюстрация к статье о GPT-4 и Openai',
+            date: 'Сент 26, 2021',
+            title: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
+            link: '#',
+        }
+    ],
     smallBlocks: [
         {
             imgSrc: './assets/img/2_4.png',
             alt: 'Первая маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
-            text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
+            title: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         },
         {
             imgSrc: './assets/img/3_4.png',
             alt: 'Вторая маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
-            text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
+            title: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         },
         {
             imgSrc: './assets/img/4_4.png',
             alt: 'Третья маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
-            text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
+            title: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         },
         {
             imgSrc: './assets/img/5_4.png',
             alt: 'Четвёртая маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
-            text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
+            title: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         }
     ]
