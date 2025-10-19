6659a54a5b86685c0aca38a6fc388c4c6b368d4a
olgs0011 <144258663+olgs0011@users.noreply.github.com>
2025-10-19 18:07:14 +0500

    Update blogData.js - добалвены alt

--- src/js/mockData/blogData.js
+++ src/js/mockData/blogData.js
@@ -1,6 +1,7 @@
 export const blogData = {
     largeBlock: {
         imgSrc: './assets/img/1_4.png',
+        alt: 'Большая иллюстрация к статье о GPT-4 и Openai',
         date: 'Сент 26, 2021',
         text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
         link: '#',
@@ -8,24 +9,28 @@ export const blogData = {
     smallBlocks: [
         {
             imgSrc: './assets/img/2_4.png',
+            alt: 'Первая маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
             text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         },
         {
             imgSrc: './assets/img/3_4.png',
+            alt: 'Вторая маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
             text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         },
         {
             imgSrc: './assets/img/4_4.png',
+            alt: 'Третья маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
             text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
         },
         {
             imgSrc: './assets/img/5_4.png',
+            alt: 'Четвёртая маленькая иллюстрация к статье о GPT-4 и Openai',
             date: 'Сент 26, 2021',
             text: 'GPT-4 и Openai – это будущее. Давайте разберемся, как это?',
             link: '#'
