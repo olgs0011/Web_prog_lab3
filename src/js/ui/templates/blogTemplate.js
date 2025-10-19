624109653909243d3860bc6977d9d081c4e36a66
olgs0011 <144258663+olgs0011@users.noreply.github.com>
2025-10-19 18:22:21 +0500

    Update blogTemplate.js

--- src/js/ui/templates/blogTemplate.js
+++ src/js/ui/templates/blogTemplate.js
@@ -1,30 +1,30 @@
 export const blogTemplate = (blogData) => {
-    const largeBlockHTML = `
+    const largeBlocksHTML = blogData.largeBlocks.map(block => `
         <div class="large_block">
-            <img class="large_img" src="${blogData.largeBlock.imgSrc}" />
+            <img class="large_img" src="${block.imgSrc}" alt="${block.alt}" />
             <div class="blok_large">
-                <p class="data_big">${blogData.largeBlock.date}</p>
-                <p class="text_big">${blogData.largeBlock.text}</p>
-                <a href="${blogData.largeBlock.link}" class="ssylka_eto_futare">Читать полную статью</a>
+                <p class="data_big">${block.date}</p>
+                <p class="text_big">${block.title}</p>
+                <a href="${block.link}" class="ssylka_eto_futare">Читать полную статью</a>
             </div>
         </div>
-    `;
+    `).join('');
 
     const smallBlocksHTML = blogData.smallBlocks.map(block => `
         <div class="text_and_img_small">
-            <img class="photo" src="${block.imgSrc}" />
+            <img class="photo" src="${block.imgSrc}" alt="${block.alt}" />
             <div class="blok_small">
                 <p class="data_small">${block.date}</p>
-                <p class="text_small">${block.text}</p>
+                <p class="text_small">${block.title}</p>
                 <a href="${block.link}" class="ssylka_eto_futare">Читать полную статью</a>
             </div>
         </div>
     `).join('');
 
     return `
-        <h2 class="header_blog">Многое Происходит, Мы Ведем об Этом Блог.</h2>
+        <h2 class="header_blog">${blogData.pageTitle}</h2>
         <div class="container_for_block">
-            ${largeBlockHTML}
+            ${largeBlocksHTML}
             <div class="small_blocks">${smallBlocksHTML}</div>
         </div>
     `;
